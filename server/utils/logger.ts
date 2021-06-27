d5de1402532e01b87692f320976b7924f062c0f2
YenHub <gilkes@gmx.com>
2021-06-27 18:55:57 +0100

    🔨 Add sequelize and create DB Client

--- server/utils/logger.ts
+++ server/utils/logger.ts
@@ -1,26 +1,29 @@
-type MessageSource = '[TestAPI]';
+export enum MessageSources {
+    TestAPI = '[TestAPI]',
+    DBClient = '[DBClient]',
+}
 
-const log = (msgSource: MessageSource) => {
+export const Log = (msgSource: MessageSources) => {
     return (msg: string) => {
         const date = new Date().toLocaleTimeString();
         console.log(`\x1b[32m${date} ${msgSource} ${msg}\x1b[0m`);
     };
 };
 
-const warn = (msgSource: MessageSource) => {
+export const Warn = (msgSource: MessageSources) => {
     return (msg: string) => {
         const date = new Date().toLocaleTimeString();
         console.log(`\x1b[33m${date} ${msgSource} ${msg}\x1b[0m`);
     };
 };
 
-const error = (msgSource: MessageSource) => {
+export const Err = (msgSource: MessageSources) => {
     return (msg: string) => {
         const date = new Date().toLocaleTimeString();
         console.log(`\x1b[31m${date} ${msgSource} ${msg}\x1b[0m`);
     };
 };
 
-export const apiLog = log('[TestAPI]');
-export const apiWarn = warn('[TestAPI]');
-export const apiError = error('[TestAPI]');
+export const apiLog = Log(MessageSources.TestAPI);
+export const apiWarn = Warn(MessageSources.TestAPI);
+export const apiError = Err(MessageSources.TestAPI);
