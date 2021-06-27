bdfde118f1bbc68e90da85d5fbab34fec0a5969e
YenHub <gilkes@gmx.com>
2021-06-27 17:16:12 +0100

    🔨 Add helmet

--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import helmet from 'helmet';
 import createError from 'http-errors';
 import path from 'path';
 import cookieParser from 'cookie-parser';
@@ -22,6 +23,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(cors());
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
