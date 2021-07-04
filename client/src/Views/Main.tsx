481a520464a8811d0ae3f99a90748c8c768ce33d
YenHub <gilkes@gmx.com>
2021-07-04 20:18:47 +0100

    🔨 Move styles into separate file

--- client/src/Views/Main.tsx
+++ client/src/Views/Main.tsx
@@ -2,8 +2,8 @@ import { FC } from 'react';
 import 'typeface-dosis';
 import clsx from 'clsx';
 import { useContext, useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 
+import { useStyles } from './Main.Styles';
 import { store, ToggleTypes } from '../Services/State/Store';
 
 import {
@@ -31,76 +31,6 @@ import ShareButtons from './Components/ShareButtons';
 import NoteStore from '../Services/Database/NoteStore';
 import { bigLog, groupLog, showGatedFeatures } from '../Services/ReactUtils';
 export const noteStore = new NoteStore();
-
-const drawerWidth = 240;
-
-const useStyles = (darkMode: boolean) => makeStyles(theme => ({
-    root: {
-        display: 'flex',
-    },
-    appBar: {
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-    },
-    appBarShift: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-        marginLeft: drawerWidth,
-    },
-    title: {
-        margin: 'auto', // Center Logo
-        // paddingRight: '24px', // Offset for Menu Icon
-        fontSize: '3em',
-        color: darkMode ? '#08d2ff' : '#007bff',
-        fontFamily: [
-            'Dosis',
-            'serif',
-        ].join(','),
-    },
-    toolBar: {
-        backgroundColor: darkMode ? '#303030' : 'white',
-    },
-    hide: {
-        display: 'none',
-    },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-    },
-    drawerPaper: {
-        width: isMobile ? '100%' : drawerWidth,
-    },
-    drawerHeader: {
-        display: 'flex',
-        alignItems: 'center',
-        padding: theme.spacing(0, 1),
-        // necessary for content to be below app bar
-        ...theme.mixins.toolbar,
-        justifyContent: 'flex-end',
-    },
-    content: {
-        flexGrow: 1,
-        padding: isMobile ? 0 : theme.spacing(3),
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        marginLeft: -drawerWidth,
-        paddingTop: 0,
-    },
-    contentShift: {
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-        marginLeft: 0,
-    },
-}));
 
 const Main: FC = () => {
 
