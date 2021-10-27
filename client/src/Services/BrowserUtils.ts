46a46a501525d49b3ac10c92e2efefb625471f01
YenHub <gilkes@gmx.com>
2021-10-27 21:32:27 +0100

    🧹 Linting

--- client/src/Services/BrowserUtils.ts
+++ client/src/Services/BrowserUtils.ts
@@ -3,11 +3,11 @@ const isIE = () => navigator.userAgent.indexOf('MISE ') !== -1 || !!navigator.us
 export const downloadFile = (content: string): void => {
     const _fileName = `QuickList-${new Date().toLocaleDateString().replace(/\//g, '-')}.txt`;
     const blob = new Blob([content], { type: 'text/plain;charset=utf-8;' });
-    if (isIE() && window.navigator.msSaveOrOpenBlob) {
+    if (isIE() && (window as any).navigator.msSaveOrOpenBlob) {
         // for IE versions 10+
         const blobObject = new Blob([content]);
         // Download using MS msSaveOrOpenBlob
-        window.navigator.msSaveOrOpenBlob(blobObject, _fileName);
+        (window as any).navigator.msSaveOrOpenBlob(blobObject, _fileName);
     } else {
         // All other browsers
         const link = document.createElement('a');
