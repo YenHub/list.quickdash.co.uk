6eed022021edf990f47cca0576fc5cea52856fc2
YenHub <gilkes@gmx.com>
2022-02-05 20:42:37 +0000

    🔀 Replace tslint with eslint & prettier

---
+++ server/prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  printWidth: 90,
+  '[yaml]': {
+    'editor.autoIndent': 'advanced',
+  },
+}
