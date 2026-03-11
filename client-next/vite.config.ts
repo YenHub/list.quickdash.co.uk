ad85f47da21ecfd4df575f0256713b5212f5680b
YenHub <gilkes@gmx.com>
2026-03-11 11:59:42 +0000

    chore: configure service workers via VitePWA

--- client-next/vite.config.ts
+++ client-next/vite.config.ts
@@ -1,7 +1,29 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        navigateFallback: '/index.html',
+        navigateFallbackDenylist: [/^\/_/, /\/[^/?]+\.[^/]+$/],
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => url.pathname.endsWith('.png'),
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'images',
+              expiration: {
+                maxEntries: 50,
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
 })
